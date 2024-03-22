91e51073c028a5ab03a77747bcbf3f0652b9674a
Dougy Fresh <dougrouseltw@gmail.com>
2024-03-21 19:17:09 -0600

    added css styling, updated file location

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid'); // For generating unique IDs
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -7,9 +11,25 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Routes
+// Placeholder for routes
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
+
+// Implementing DELETE request
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+  
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      const updatedNotes = notes.filter(note => note.id !== noteId);
+  
+      fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(updatedNotes, null, 2), err => {
+        if (err) throw err;
+        res.json({ message: 'Deleted note' });
+      });
+    });
+});
 
 // Server listening
 app.listen(PORT, () => {
